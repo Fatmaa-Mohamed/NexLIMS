2a19f84a331328e3a1af8acc821f7b5c5833a0af
mahmoud <mahmoudrdwan32@gmail.com>
2026-06-13 10:12:37 +0300

    resolve git

--- NexLIMS.API/Program.cs
+++ NexLIMS.API/Program.cs
@@ -21,25 +21,6 @@ using NextLIMS.DAL.Repository.Department;
 using NextLIMS.DAL.Repository.SampleRepo;
 using System.Text;
 
-<<<<<<< Updated upstream
-=======
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using NexLIMS.API.Controllers.middlewares;
-using NextLIMS.BLL.Services.Auth;
-using NextLIMS.BLL.Services.EmailService;
-using NextLIMS.BLL.Services.EmployeeService;
-using NextLIMS.BLL.Services.Invitation;
-using NextLIMS.BLL.Services.Permissions;
-using NextLIMS.BLL.Services.Roles;
-using NextLIMS.BLL.Services.SignupService;
-using NextLIMS.DAL.Data;
-using NextLIMS.DAL.Data.Payment;
-using NextLIMS.DAL.Repositories;
-using System.Text;
-
->>>>>>> Stashed changes
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -48,17 +29,7 @@ builder.Services.AddControllers(
     options => options.Filters.Add<PermissionBasedAuthFilter>()
     );
 builder.Services.AddHttpContextAccessor();
-<<<<<<< Updated upstream
 builder.Services.AddControllers();
-=======
-builder.Services.AddControllers(
-    options => options.Filters.Add<PermissionBasedAuthFilter>()
-    );
-builder.Services.Configure<FawaterkSettings>(builder.Configuration.GetSection("Fawaterk"));
-builder.Services.AddHttpClient();
-builder.Services.Configure<FawaterkSettings>(builder.Configuration.GetSection("Fawaterk"));
-builder.Services.AddHttpContextAccessor();
->>>>>>> Stashed changes
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
