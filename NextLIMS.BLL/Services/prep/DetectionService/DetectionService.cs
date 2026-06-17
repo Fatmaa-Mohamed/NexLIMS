414b2af1fee947d3ca30a1ece79aa4ee8f4014de
Saikoo <sayed.ahmedd.m@gmail.com>
2026-06-17 03:01:49 +0300

    handle Sample workflow in Analyst

--- NextLIMS.BLL/Services/prep/DetectionService/DetectionService.cs
+++ NextLIMS.BLL/Services/prep/DetectionService/DetectionService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using NextLIMS.BLL.DTO.PrepDTOs;
 using NextLIMS.DAL.Data;
 using NextLIMS.DAL.Data.Models;
@@ -15,10 +16,13 @@ namespace NextLIMS.BLL.Services.prep.DetectionService
     public class DetectionService : IDetectionService
     {
         private readonly IGenericRepository<DetectionData> _repo;
+        private readonly ApplicationDbContext _context; // Added to manage queries and transactions
         private readonly IHttpContextAccessor _httpContextAccessor;
-        public DetectionService(IGenericRepository<DetectionData> repository, IHttpContextAccessor httpContextAccessor)
+
+        public DetectionService(IGenericRepository<DetectionData> repository, ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
         {
             _repo = repository;
+            _context = context; // Initialized
             _httpContextAccessor = httpContextAccessor;
         }
 
@@ -27,17 +31,45 @@ namespace NextLIMS.BLL.Services.prep.DetectionService
             var createdBy = int.Parse(_httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
             var tenantId = int.Parse(_httpContextAccessor.HttpContext.User.FindFirst("TenantId").Value);
 
-            var detectionData = new DetectionData
+            using var transaction = await _context.Database.BeginTransactionAsync();
+            try
             {
-                SampleTestId = sampleTestId,
-                TenantId = tenantId,
-                Weight = dto.Weight,
-                EnrichmentMedia = dto.EnrichmentMedia,
-                MediaAmount = dto.MediaAmount,
-                CreatedAt = DateTime.UtcNow,
-                CreatedBy = createdBy
-            };
-            return await _repo.AddAsync(detectionData);
+                var sampleTest = await _context.SampleTests
+                                               .Include(st => st.Sample)
+                                               .FirstOrDefaultAsync(st => st.Id == sampleTestId);
+
+                if (sampleTest == null) throw new Exception("Sample Test not found.");
+
+                var detectionData = new DetectionData
+                {
+                    SampleTestId = sampleTestId,
+                    TenantId = tenantId,
+                    Weight = dto.Weight,
+                    EnrichmentMedia = dto.EnrichmentMedia,
+                    MediaAmount = dto.MediaAmount,
+                    CreatedAt = DateTime.UtcNow,
+                    CreatedBy = createdBy
+                };
+
+                await _repo.AddAsync(detectionData);
+
+                var workflow = await _context.SampleWorkflows
+                                             .FirstOrDefaultAsync(w => w.SampleId == sampleTest.SampleId && w.TenantId == tenantId);
+                if (workflow != null)
+                {
+                    workflow.Action = "inprogress";
+                    workflow.StartDate = workflow.EndDate;
+                    _context.SampleWorkflows.Update(workflow);
+                }
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return detectionData.Id;
+            }
+            catch (Exception)
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
         }
     }
 }
--- NextLIMS.BLL/Services/prep/EnumerationService/EnumerationService.cs
+++ NextLIMS.BLL/Services/prep/EnumerationService/EnumerationService.cs
@@ -18,71 +18,85 @@ namespace NextLIMS.BLL.Services.prep.EnumerationService
         private readonly IGenericRepository<EnumerationData> _repo;
         private readonly ApplicationDbContext _context;
         private readonly IHttpContextAccessor _httpContextAccessor;
-        public EnumerationService(IGenericRepository<EnumerationData> repository,ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
+        public EnumerationService(IGenericRepository<EnumerationData> repository, ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
         {
             _repo = repository;
             _context = context;
             _httpContextAccessor = httpContextAccessor;
         }
-
-
         public async Task<int> SaveEnumerationPrepAsync(int sampleTestId, EnumerationPrepDto dto)
         {
             var createdBy = int.Parse(_httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
             var tenantId = int.Parse(_httpContextAccessor.HttpContext.User.FindFirst("TenantId").Value);
 
-            var enumerationData = new EnumerationData
+            using var transaction = await _context.Database.BeginTransactionAsync();
+            try
             {
-                SampleTestId = sampleTestId,
-                TenantId = tenantId,
-                Weight = dto.Weight,
-                DiluentAmount = dto.DiluentAmount,
-                CreatedAt = DateTime.UtcNow,
-                CreatedBy = createdBy
-            };
+                var sampleTest = await _context.SampleTests
+                                               .Include(st => st.Sample)
+                                               .FirstOrDefaultAsync(st => st.Id == sampleTestId);
 
-            await _repo.AddAsync(enumerationData);
+                if (sampleTest == null) throw new Exception("Sample Test not found.");
 
-            string? sampleType = await _context.SampleTests
-                                                .Where(st => st.Id == sampleTestId)
-                                                .Select(st => st.Sample.SampleType)
-                                                .FirstOrDefaultAsync();
-
-            string? dilutionType = null;
-            if (!string.IsNullOrEmpty(sampleType))
-            {
-                var cleanType = sampleType.Trim().ToLower(); 
-                if (cleanType == "water")
-                    dilutionType = "Volume";
-                else if (cleanType == "swab" || cleanType == "soil" || cleanType == "food")
-                    dilutionType = "dilution";
-            }
-
-            if (dto.dilutions != null && dto.dilutions.Any())
-            {
-                var dilutionEntities = new List<EnumerationDilution>();
-
-                foreach (var dilutionDto in dto.dilutions)
+                var enumerationData = new EnumerationData
                 {
-                    dilutionEntities.Add(new EnumerationDilution
-                    {
-                        EnumerationDataId = enumerationData.Id, 
-                        TenantId = tenantId,
-                        DilutionOrVolume = dilutionDto.DilutionOrVolume,
-                        VolumePlated = dilutionDto.VolumePlated,
-                        DilutionType = dilutionType,
-                        IsSelectedForCalculation = false,
-                        CreatedAt = DateTime.UtcNow,
-                        CreatedBy = createdBy
-                    });
+                    SampleTestId = sampleTestId,
+                    TenantId = tenantId,
+                    Weight = dto.Weight,
+                    DiluentAmount = dto.DiluentAmount,
+                    CreatedAt = DateTime.UtcNow,
+                    CreatedBy = createdBy
+                };
+
+                await _repo.AddAsync(enumerationData);
+
+                string? dilutionType = null;
+                if (sampleTest.Sample != null && !string.IsNullOrEmpty(sampleTest.Sample.SampleType))
+                {
+                    var cleanType = sampleTest.Sample.SampleType.Trim().ToLower();
+                    if (cleanType == "water") dilutionType = "Volume";
+                    else if (cleanType == "swab" || cleanType == "soil" || cleanType == "food") dilutionType = "dilution";
                 }
 
-                
-                await _context.EnumerationDilutions.AddRangeAsync(dilutionEntities);
-                await _context.SaveChangesAsync();
-            }
+                if (dto.dilutions != null && dto.dilutions.Any())
+                {
+                    var dilutionEntities = new List<EnumerationDilution>();
+                    foreach (var dilutionDto in dto.dilutions)
+                    {
+                        dilutionEntities.Add(new EnumerationDilution
+                        {
+                            EnumerationDataId = enumerationData.Id, 
+                            TenantId = tenantId,
+                            DilutionOrVolume = dilutionDto.DilutionOrVolume,
+                            VolumePlated = dilutionDto.VolumePlated,
+                            DilutionType = dilutionType,
+                            IsSelectedForCalculation = false,
+                            CreatedAt = DateTime.UtcNow,
+                            CreatedBy = createdBy
+                        });
+                    }
+                    await _context.EnumerationDilutions.AddRangeAsync(dilutionEntities);
+                }
 
-            return enumerationData.Id;
+                var workflow = await _context.SampleWorkflows.FirstOrDefaultAsync(w => w.SampleId == sampleTest.SampleId && w.TenantId == tenantId);
+                if (workflow != null)
+                {
+                    workflow.Action = "inprogress";
+                    workflow.StartDate = workflow.EndDate;
+                    _context.SampleWorkflows.Update(workflow);
+                }
+
+                await _context.SaveChangesAsync();
+
+                await transaction.CommitAsync();
+
+                return enumerationData.Id;
+            }
+            catch (Exception)
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
         }
     }
 }
